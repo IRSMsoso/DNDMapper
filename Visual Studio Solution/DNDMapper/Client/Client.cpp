c63005a1dc83440faaab82257786023695cda8d5
Cameron VanderTuig <camvandertuig@gmail.com>
2020-06-01 22:45:31 -0500

    Setting up SFML dependency settings

--- Visual Studio Solution/DNDMapper/Client/Client.cpp
+++ Visual Studio Solution/DNDMapper/Client/Client.cpp
@@ -1,9 +1,10 @@
-// Client.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Client.cpp : Client Main File
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	
+	return 0;
 }
