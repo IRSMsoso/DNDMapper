46a95463b742e959135e39560fddf297fd958fa8
Cameron VanderTuig <camvandertuig@gmail.com>
2020-06-02 23:05:15 -0500

    Doesn't quite work

--- Visual Studio Solution/DNDMapper/Client/Manager.h
+++ Visual Studio Solution/DNDMapper/Client/Manager.h
@@ -1,7 +1,51 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include "Canvas.h"
+#include <iostream>
+
+
+const unsigned int WINDOWX = 800;
+const unsigned int WINDOWY = 800;
+const float MAXZOOM = .5;
+const float MINZOOM = 1.5;
+const float ZOOMSPEED = 0.9;
+const float BEADRADIUS = 0.3;
+
 class Manager{
 public:
-	Manager();
+	Manager(sf::RenderWindow*);
 	~Manager();
+
+	//Main loop. Called as its own thread.
+	void mainLoop();
+
+	void addEvent(sf::Event);
+
+private:
+	//Window
+	sf::RenderWindow* window;
+
+	//Camera View
+	sf::View camera;
+
+	//Navigation Tools
+	bool isPanning = false;
+	sf::Vector2f panLockLoc;
+	float zoomFactor;
+
+	//Canvas
+	Canvas canvas;
+
+	//Cursors
+	sf::Cursor handCursor, defaultCursor;
+
+	//Event Queue
+	std::vector<sf::Event> eventQueue;
+
+	//Event interpret
+	void interpretEvent(sf::Event);
+
+	//Mutex
+	sf::Mutex mutex;
 };
 
